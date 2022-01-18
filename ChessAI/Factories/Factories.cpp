2e0ea7654a816e3def6174ecbcf0b0dcacbcd578
Rhidian De Wit <rhidiandewit@gmail.com>
2022-01-18 22:06:22 +0100

    started work on factory

--- ChessAI/Factories/Factories.cpp
+++ ChessAI/Factories/Factories.cpp
@@ -1,1 +1,30 @@
 #include "Factories.h"
+
+#include <GameObject/GameObject.h>
+#include <Core/Core.h>
+#include <Components/TextureComponent/TextureComponent.h>
+#include <TextureManager/TextureManager.h>
+
+namespace Factories
+{
+	std::vector<Integrian2D::GameObject*> CreateChessboard() noexcept
+	{
+		using namespace Integrian2D;
+
+		/* Chessboard has 8x8 tiles */
+
+		const Point2f windowDimensions{ Core::GetInstance()->GetWindowWidth(), Core::GetInstance()->GetWindowHeight() };
+
+		const Point2f tileDimensions{ windowDimensions.x / 8.f, windowDimensions.y / 8.f };
+
+		std::vector<GameObject*> m_Tiles{};
+
+		for (int x{}; x < 8; ++x)
+		{
+			for (int y{}; y < 8; ++y)
+			{
+				GameObject* pTile{ new GameObject{} };
+			}
+		}
+	}
+}
--- ChessAI/Factories/Factories.h
+++ ChessAI/Factories/Factories.h
@@ -1,7 +1,14 @@
 #pragma once
 
+#include <vector>
+
 namespace Integrian2D
 {
 	class GameObject;
 }
 
+namespace Factories
+{
+	/* [TODO] Finish function when Pieces are implemented */
+	[[nodiscard]] std::vector<Integrian2D::GameObject*> CreateChessboard() noexcept;
+}
