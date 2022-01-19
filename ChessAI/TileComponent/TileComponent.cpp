b080d5d8c8d9bb5b65edb9cd723b2e27744a3572
Rhidian De Wit <rhidiandewit@gmail.com>
2022-01-19 20:14:05 +0100

    made tilecomponent, not finished

--- ChessAI/TileComponent/TileComponent.cpp
+++ ChessAI/TileComponent/TileComponent.cpp
@@ -1,1 +1,20 @@
 #include "TileComponent.h"
+
+#include <GameObject/GameObject.h>
+#include <Components/TransformComponent/TransformComponent.h>
+#include <Renderer/Renderer.h>
+
+TileComponent::TileComponent(Integrian2D::GameObject* const pOwner, const float tileWidth, const float tileHeight, Integrian2D::Texture* const pTexture)
+	: Component{ pOwner }
+	, m_TileWidth{ tileWidth }
+	, m_TileHeight{ tileHeight }
+	, m_pTexture{ pTexture }
+{
+}
+
+void TileComponent::Render() const
+{
+	using namespace Integrian2D;
+
+	Renderer::GetInstance()->RenderTexture(m_pTexture, PRectf{ m_pOwner->pTransform->GetWorldPosition(), m_TileWidth, m_TileHeight }, Rectf{});
+}
--- ChessAI/TileComponent/TileComponent.h
+++ ChessAI/TileComponent/TileComponent.h
@@ -2,11 +2,22 @@
 
 #include <Components/Component/Component.h>
 
+namespace Integrian2D
+{
+	class Texture;
+}
+
 class TileComponent final : public Integrian2D::Component
 {
 public:
+	TileComponent(Integrian2D::GameObject* const pOwner, const float tileWidth, const float tileHeight, Integrian2D::Texture* const pTexture);
 
+	virtual void Render() const override;
 
 private:
+	/* [TODO] Add reference to piece in here */
+	float m_TileWidth;
+	float m_TileHeight;
 
+	Integrian2D::Texture* m_pTexture;
 };
