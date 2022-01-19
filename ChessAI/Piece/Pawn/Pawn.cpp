7c24bfc6c2f7a66f25a57a7ae09604aba259b03a
Rhidian De Wit <rhidiandewit@gmail.com>
2022-01-19 23:34:41 +0100

    added Pawn class

---
+++ ChessAI/Piece/Pawn/Pawn.cpp
@@ -0,0 +1,6 @@
+#include "Pawn.h"
+
+Pawn::Pawn(Integrian2D::GameObject* const pOwner)
+	: Piece{ pOwner, TypeOfPiece::Pawn }
+{
+}
---
+++ ChessAI/Piece/Pawn/Pawn.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "../Piece.h"
+
+class Pawn final : public Piece
+{
+public:
+	Pawn(Integrian2D::GameObject* const pOwner);
+
+private:
+
+};
