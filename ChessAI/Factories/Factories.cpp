15c93b9b1f1aac314858532da0bbc6d80cf29281
Rhidian De Wit <rhidiandewit@gmail.com>
2022-01-19 20:18:27 +0100

    Chessboard factory made

--- ChessAI/Factories/Factories.cpp
+++ ChessAI/Factories/Factories.cpp
@@ -4,6 +4,9 @@
 #include <Core/Core.h>
 #include <Components/TextureComponent/TextureComponent.h>
 #include <TextureManager/TextureManager.h>
+#include <Components/TransformComponent/TransformComponent.h>
+
+#include "../TileComponent/TileComponent.h"
 
 namespace Factories
 {
@@ -17,14 +20,26 @@ namespace Factories
 
 		const Point2f tileDimensions{ windowDimensions.x / 8.f, windowDimensions.y / 8.f };
 
-		std::vector<GameObject*> m_Tiles{};
+		std::vector<GameObject*> tiles{};
 
+		int counter{};
 		for (int x{}; x < 8; ++x)
 		{
 			for (int y{}; y < 8; ++y)
 			{
 				GameObject* pTile{ new GameObject{} };
+
+				if (counter % 2 == 0)
+					pTile->AddComponent(new TileComponent{ pTile, tileDimensions.x, tileDimensions.y, TextureManager::GetInstance()->GetTexture("WhiteTile") });
+				else
+					pTile->AddComponent(new TileComponent{ pTile, tileDimensions.x, tileDimensions.y, TextureManager::GetInstance()->GetTexture("BlackTile") });
+
+				pTile->pTransform->SetPosition(Point2f{ x * tileDimensions.x, y * tileDimensions.y });
+
+				tiles.push_back(pTile);
 			}
 		}
+
+		return tiles;
 	}
 }
