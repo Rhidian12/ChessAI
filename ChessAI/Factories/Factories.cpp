b4c84e73394737946a20b5fde3acb973b4f4aa60
Rhidian De Wit <74491212+Rhidian12@users.noreply.github.com>
2022-02-01 16:38:51 +0100

    added Knight to factories

--- ChessAI/Factories/Factories.cpp
+++ ChessAI/Factories/Factories.cpp
@@ -12,6 +12,7 @@
 #include "../Piece/Bishop/Bishop.h"
 #include "../Piece/Queen/Queen.h"
 #include "../Piece/King/King.h"
+#include "../Piece/Knight/Knight.h"
 
 namespace Factories
 {
@@ -53,14 +54,20 @@ namespace Factories
 			/* Add white pawns */
 			Texture* const pWhitePawn{ pTextureManager->GetTexture("WhitePawn") };
 			for (int i{ 8 }; i < 16; ++i)
+			{
 				tiles[i]->AddComponent(new Pawn{ tiles[i], pWhitePawn });
+				tiles[i]->GetComponentByType<Pawn>()->SetColourOfPiece(PieceColour::White);
+			}
 		}
 
 		{
 			/* Add black pawns */
 			Texture* const pBlackPawn{ pTextureManager->GetTexture("BlackPawn") };
 			for (int i{ 48 }; i < 56; ++i)
+			{
 				tiles[i]->AddComponent(new Pawn{ tiles[i], pBlackPawn });
+				tiles[i]->GetComponentByType<Pawn>()->SetColourOfPiece(PieceColour::Black);
+			}
 		}
 #pragma endregion
 
@@ -69,14 +76,20 @@ namespace Factories
 			/* Add White Rooks */
 			Texture* const pWhiteRook{ pTextureManager->GetTexture("WhiteRook") };
 			tiles[0]->AddComponent(new Rook{ tiles[0], pWhiteRook });
+			tiles[0]->GetComponentByType<Rook>()->SetColourOfPiece(PieceColour::White);
+
 			tiles[7]->AddComponent(new Rook{ tiles[7], pWhiteRook });
+			tiles[7]->GetComponentByType<Rook>()->SetColourOfPiece(PieceColour::White);
 		}
 
 		{
 			/* Add Black Rooks */
 			Texture* const pBlackRook{ pTextureManager->GetTexture("BlackRook") };
 			tiles[56]->AddComponent(new Rook{ tiles[56], pBlackRook });
+			tiles[56]->GetComponentByType<Rook>()->SetColourOfPiece(PieceColour::Black);
+
 			tiles[63]->AddComponent(new Rook{ tiles[63], pBlackRook });
+			tiles[63]->GetComponentByType<Rook>()->SetColourOfPiece(PieceColour::Black);
 		}
 #pragma endregion
 
@@ -85,14 +98,42 @@ namespace Factories
 			/* Add White Bishops */
 			Texture* const pWhiteBishop{ pTextureManager->GetTexture("WhiteBishop") };
 			tiles[2]->AddComponent(new Bishop{ tiles[2], pWhiteBishop });
+			tiles[2]->GetComponentByType<Bishop>()->SetColourOfPiece(PieceColour::White);
+
 			tiles[5]->AddComponent(new Bishop{ tiles[5], pWhiteBishop });
+			tiles[5]->GetComponentByType<Bishop>()->SetColourOfPiece(PieceColour::White);
 		}
 
 		{
 			/* Add Black Bishops */
 			Texture* const pBlackBishop{ pTextureManager->GetTexture("BlackBishop") };
-			tiles[58]->AddComponent(new Rook{ tiles[58], pBlackBishop });
-			tiles[61]->AddComponent(new Rook{ tiles[61], pBlackBishop });
+			tiles[58]->AddComponent(new Bishop{ tiles[58], pBlackBishop });
+			tiles[58]->GetComponentByType<Bishop>()->SetColourOfPiece(PieceColour::Black);
+
+			tiles[61]->AddComponent(new Bishop{ tiles[61], pBlackBishop });
+			tiles[61]->GetComponentByType<Bishop>()->SetColourOfPiece(PieceColour::Black);
+		}
+#pragma endregion
+
+#pragma region Knights
+		{
+			/* Add White Knights */
+			Texture* const pWhiteKnight{ pTextureManager->GetTexture("WhiteKnight") };
+			tiles[3]->AddComponent(new Knight{ tiles[3], pWhiteKnight });
+			tiles[3]->GetComponentByType<Knight>()->SetColourOfPiece(PieceColour::White);
+
+			tiles[6]->AddComponent(new Knight{ tiles[6], pWhiteKnight });
+			tiles[6]->GetComponentByType<Knight>()->SetColourOfPiece(PieceColour::White);
+		}
+
+		{
+			/* Add Black Knights */
+			Texture* const pBlackKnight{ pTextureManager->GetTexture("BlackKnight") };
+			tiles[59]->AddComponent(new Knight{ tiles[59], pBlackKnight });
+			tiles[59]->GetComponentByType<Knight>()->SetColourOfPiece(PieceColour::Black);
+
+			tiles[62]->AddComponent(new Knight{ tiles[62], pBlackKnight });
+			tiles[62]->GetComponentByType<Knight>()->SetColourOfPiece(PieceColour::Black);
 		}
 #pragma endregion
 
@@ -101,12 +142,14 @@ namespace Factories
 			/* Add White Queen */
 			Texture* const pWhiteQueen{ pTextureManager->GetTexture("WhiteQueen") };
 			tiles[4]->AddComponent(new Queen{ tiles[4], pWhiteQueen });
+			tiles[4]->GetComponentByType<Queen>()->SetColourOfPiece(PieceColour::White);
 		}
 
 		{
 			/* Add Black Queen */
 			Texture* const pBlackQueen{ pTextureManager->GetTexture("BlackQueen") };
 			tiles[60]->AddComponent(new Queen{ tiles[60], pBlackQueen });
+			tiles[60]->GetComponentByType<Queen>()->SetColourOfPiece(PieceColour::Black);
 		}
 #pragma endregion
 
@@ -114,13 +157,15 @@ namespace Factories
 		{
 			/* Add White King */
 			Texture* const pWhiteKing{ pTextureManager->GetTexture("WhiteKing") };
-			tiles[3]->AddComponent(new Queen{ tiles[3], pWhiteKing });
+			tiles[3]->AddComponent(new King{ tiles[3], pWhiteKing });
+			tiles[3]->GetComponentByType<King>()->SetColourOfPiece(PieceColour::White);
 		}
 
 		{
 			/* Add Black King */
 			Texture* const pBlackKing{ pTextureManager->GetTexture("BlackKing") };
-			tiles[59]->AddComponent(new Queen{ tiles[59], pBlackKing });
+			tiles[59]->AddComponent(new King{ tiles[59], pBlackKing });
+			tiles[59]->GetComponentByType<King>()->SetColourOfPiece(PieceColour::Black);
 		}
 #pragma endregion
 
